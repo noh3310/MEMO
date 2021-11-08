d9c0b004e1f5562e9b9ea97ad7eb9113dbd69e12
GunHo <noh3310@gmail.com>
2021-11-08 20:37:52 +0900

    first commit

---
+++ SeSac_Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSac_Memo
+//
+//  Created by 노건호 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
